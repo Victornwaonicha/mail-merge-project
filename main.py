cf61aaf5f354f23fe8d97c47643debe34a2adf20
Victor <victornwaonicha@gmail.com>
2025-08-02 03:00:35 -0700

    Mail-Merging

--- main.py
+++ main.py
@@ -3,22 +3,24 @@
 # Replace the [name] placeholder with the actual name.
 # Save the letters in the folder "ReadyToSend".
 
-
+READ_FILE = "r"
+WRITE_TO_FILE = "w"
 stripped_names = []
-with open("Input/Names/invited_names.txt", "r") as file:
+
+with open("Input/Names/invited_names.txt", READ_FILE) as file:
     names = file.readlines()
     for name in names:
         str_names = name.strip()
         stripped_names.append(str_names)
 
 
-with open("Input/Letters/starting_letter.txt", "r") as file:
+with open("Input/Letters/starting_letter.txt", READ_FILE) as file:
     letter_contents = file.read()
 
 
 for name in stripped_names:
     new_letter = letter_contents.replace("[name]", name)
-    with open(f"Output/ReadyToSend/letter_for_{name}.txt", "w") as file:
+    with open(f"Output/ReadyToSend/letter_for_{name}.txt", WRITE_TO_FILE) as file:
         letters = file.write(new_letter)
 
 
